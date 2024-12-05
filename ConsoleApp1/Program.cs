bd346e3622a0a98d4396602c44c295eccab88ec7
jyena1224 <jyena@kfa.biglobe.ne.jp>
2024-12-05 16:58:21 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+class Program
+{
+    private static LowLevelMouseProc _mouseProc = HookCallback;
+    private static IntPtr _hookId = IntPtr.Zero;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("マウスキーダウンを記録しています。終了するには Ctrl+C を押してください。");
+
+        _hookId = SetHook(_mouseProc);
+        Application.Run(); // メッセージループを開始
+
+        UnhookWindowsHookEx(_hookId); // アプリケーション終了時にフックを解除
+    }
+
+    private static IntPtr SetHook(LowLevelMouseProc proc)
+    {
+        using (Process curProcess = Process.GetCurrentProcess())
+        using (ProcessModule curModule = curProcess.MainModule)
+        {
+            return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+        }
+    }
+
+    private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0)
+        {
+            MSLLHOOKSTRUCT hookStruct = Marshal.PtrToStructure<MSLLHOOKSTRUCT>(lParam);
+
+            if (wParam == (IntPtr)WM_LBUTTONDOWN) // 左クリック
+            {
+                Console.WriteLine($"左ボタンが押されました: {hookStruct.pt.x}, {hookStruct.pt.y}");
+            }
+            else if (wParam == (IntPtr)WM_RBUTTONDOWN) // 右クリック
+            {
+                Console.WriteLine($"右ボタンが押されました: {hookStruct.pt.x}, {hookStruct.pt.y}");
+            }
+        }
+        return CallNextHookEx(_hookId, nCode, wParam, lParam);
+    }
+
+    private const int WH_MOUSE_LL = 14;
+    private const int WM_LBUTTONDOWN = 0x0201;
+    private const int WM_RBUTTONDOWN = 0x0204;
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct POINT
+    {
+        public int x;
+        public int y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct MSLLHOOKSTRUCT
+    {
+        public POINT pt;
+        public uint mouseData;
+        public uint flags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+}
